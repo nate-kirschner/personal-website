7aa1d52ea75c138fcc7943327de403910d3e6342
nate-kirschner <77256794+nate-kirschner@users.noreply.github.com>
2022-08-30 21:57:58 -0400

    changed animations and some box shadows

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,39 +1,38 @@
+import { useEffect, useState } from "react";
 import "../styles/home.scss";
+import resume from "../images/Resume.pdf";
 
 export default function Home({ allRefs, scrollToPage }) {
+
+    const [animate, setAnimate] = useState(false);
 
     return (
         <div className="homepage" ref={allRefs["Home"]}>
             <div className="titleBlock">
-                <h1 className="mainTitle">
-                    <div className="word">
-                        <div className="letter">H</div>
-                        <div className="letter">i</div>,&nbsp;
-                    </div>
-                    <div className="word">
-                        <div className="letter">I</div>'
-                        <div className="letter break">m</div>&nbsp;
-                    </div>
-                    <div className="word red">
-                        <div className="letter">N</div>
-                        <div className="letter">a</div>
-                        <div className="letter">t</div>
-                        <div className="letter break">e</div>&nbsp;
-                    </div>
-                    <div className="word">
-                        <div className="letter">K</div>
-                        <div className="letter">i</div>
-                        <div className="letter">r</div>
-                        <div className="letter">s</div>
-                        <div className="letter">c</div>
-                        <div className="letter">h</div>
-                        <div className="letter">n</div>
-                        <div className="letter">e</div>
-                        <div className="letter">r</div>.
-                    </div>
+                <h1 className={"mainTitle " + (animate && "animate")} 
+                >
+                        Hi, I'm&nbsp;
+                        <div className="letters" onMouseEnter={() => setAnimate(true)} onClick={() => setAnimate(true)}>
+                            <div className="letter">N</div>
+                            <div className="letter">a</div>
+                            <div className="letter">t</div>
+                            <div className="letter break">e</div>&nbsp;
+                            <div className="letter">K</div>
+                            <div className="letter">i</div>
+                            <div className="letter">r</div>
+                            <div className="letter">s</div>
+                            <div className="letter">c</div>
+                            <div className="letter">h</div>
+                            <div className="letter">n</div>
+                            <div className="letter">e</div>
+                            <div className="letter">r</div>
+                            .
+                        </div>
+                        
                     {/* Hi, I'm Nate Kirschner. */}
                 </h1>
                 <h2 className="titleDescription">A Computer Science Student and Aspiring Software Developer.</h2>
+                <button className="resumeButton" onClick={() => window.open(resume, '_blank', 'noopener,noreferrer')}>Resume</button>
             </div >
             <div className="menuBlock">
                 <h3 className="menuItem" onClick={() => scrollToPage(allRefs["About Me"])}>About Me</h3>
